61129fef2992c2e9efc943691bac4eecebe346bc
Daniel Fasula <danielfasula@gmail.com>
2021-03-22 09:07:11 -0600

    push

--- buglog.client/src/pages/BugsPage.vue
+++ buglog.client/src/pages/BugsPage.vue
@@ -90,15 +90,25 @@ export default {
         const table = document.getElementById('myTable')
         switching = true
         dir = 'asc'
+        /* Make a loop that will continue until
+        no switching has been done: */
         while (switching) {
+          // Start by saying: no switching is done:
           switching = false
           const rows = table.rows
+          /* Loop through all table rows (except the
+          first, which contains table headers): */
           for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
             shouldSwitch = false
+            /* Get the two elements you want to compare,
+            one from current row and one from the next: */
             x = rows[i].getElementsByTagName('TD')[n]
             y = rows[i + 1].getElementsByTagName('TD')[n]
+            // Check if the two rows should switch place:
             if (dir === 'asc') {
               if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                // If so, mark as a switch and break the loop:
                 shouldSwitch = true
                 break
               }
@@ -110,6 +120,8 @@ export default {
             }
           }
           if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
             switching = true
             switchcount++
